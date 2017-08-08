4a07c76e089076f17b57c090eafb0d4a9b191b7a
Daniel Larsen <dalars@microsoft.com>
2017-08-08 21:28:37 +1200

    Wellington DevOps talk notes - Pester

---
+++ talks/wgn-devops-lightning-pester/Smoke.Tests.ps1
@@ -0,0 +1,33 @@
+﻿# Function under test
+function DoSomethingAwesome {
+    "Something Awesome"
+}
+
+# Does something awesome
+Describe "Do something awesome"{
+    It "Does something awesome"{
+        DoSomethingAwesome | Should Be "Something awesome"
+    }
+}
+
+# Mock the DoSomethingAwesome function
+Describe "Do something awesome"{
+    Mock DoSomethingAwesome { return "Not awesome"}
+
+    It "Does something awesome"{
+        DoSomethingAwesome | Should Be "Something awesome"
+    }
+}
+
+# Smoke test the Health API - Status and Version number.
+Describe "/health API"{
+    $result = Invoke-RestMethod -Method Get -Uri https://putauaki-api.azurewebsites.net/api/health
+    
+    It "Returns Status OK"{
+        $result.Status | Should Be "Ok"
+    }
+
+    It "Should be Version 1.0.1" {
+        $result.Version | Should Be "1.0.1"
+    }
+}
