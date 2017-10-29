e68d39eedee1943ed8e85d5dfe686411ca95b604
Daniel Larsen <dalars@microsoft.com>
2017-10-29 13:52:38 +1300

    Ignore role assignment errors

--- talks/tech-hui/DeployScaffold.ps1
+++ talks/tech-hui/DeployScaffold.ps1
@@ -1,47 +1,52 @@
-﻿# ## Resource Groups
+﻿# # Deploy Scaffold
+
+[CmdletBinding()]
+param()
+
+# ## Resource Groups
 
 # Create SQL Resource Group
-Write-Verbose "Creating Resource Group example-sql-prod-aue-rg ..." -Verbose
+Write-Verbose "Creating Resource Group example-sql-prod-aue-rg ..."
 
 New-AzureRMResourceGroup -Name example-sql-prod-aue-rg -Location australiaeast `
     -Tag @{ 'cost-centre' = 'IT'; 'owner-username' = 'dalars@microsoft.com'; 'project-code' = 'SQL-Cluster' } `
-    -Force -Verbose
+    -Force
 
 # Assign sysops as owner
-Write-Verbose "Assigning Sysops Owner role..." -Verbose
+Write-Verbose "Assigning Sysops Owner role..."
 
 $sysopsGroupObjectId = 'ae7a3fdd-e1a7-4cf5-8791-00d310bbeef8'
 New-AzureRmRoleAssignment -ObjectId $sysopsGroupObjectId -RoleDefinitionName owner `
-                          -ResourceGroupName example-sql-prod-aue-rg -Verbose
+                          -ResourceGroupName example-sql-prod-aue-rg -ErrorAction SilentlyContinue
 
 # Assign SQL DBAs as contributors
-Write-Verbose "Assigning SQL DBAs Contributor role..." -Verbose
+Write-Verbose "Assigning SQL DBAs Contributor role..."
 
 $sqlDbasGroupObjectId = '7899d7b0-e5ef-487b-b002-9a86ba977f32'
 New-AzureRmRoleAssignment -ObjectId $sqlDbasGroupObjectId -RoleDefinitionName contributor `
-                          -ResourceGroupName example-sql-prod-aue-rg -Verbose
+                          -ResourceGroupName example-sql-prod-aue-rg -ErrorAction SilentlyContinue
 
 
 # Create Network Resource Group
-Write-Verbose "Creating Resource Group networking-prod-rg ..." -Verbose
+Write-Verbose "Creating Resource Group networking-prod-rg ..."
 New-AzureRMResourceGroup -Name networking-prod-rg -Location australiaeast `
     -Tag @{ 'cost-centre' = 'IT'; 'owner-username' = 'dalars@microsoft.com'; 'project-code' = 'SQL-Cluster' } `
-    -Force -Verbose
+    -Force
 
 # Assign sysops as owner
-Write-Verbose "Assigning Sysops Owner role..." -Verbose
+Write-Verbose "Assigning Sysops Owner role..."
 New-AzureRmRoleAssignment -ObjectId $sysopsGroupObjectId -RoleDefinitionName owner `
-                          -ResourceGroupName networking-prod-rg -Verbose
+                          -ResourceGroupName networking-prod-rg -ErrorAction SilentlyContinue
 
 
 # ## ARM Policies
 
 # Create a Definition
 
-Write-Verbose "Creating ARM Policy Definition..." -Verbose
+Write-Verbose "Creating ARM Policy Definition..."
 $definition = New-AzureRmPolicyDefinition -Name AllowedVNetPolicy -DisplayName 'Allowed VNets' `
                                           -Description 'Only allow scaffold VNets.' `
-                                          -Policy .\DenyNonScaffoldVnet.json -Verbose
+                                          -Policy .\DenyNonScaffoldVnet.json
 
 # Derive the scope = "/subscriptions/(subscriptionId)/resourceGroups/(resourceGroup)"
 $subscription = (Get-AzureRmContext).Subscription
@@ -51,21 +56,21 @@ $subId = if ($subscription.SubscriptionId) { $subscription.SubscriptionId } else
 $scope = "/subscriptions/$subId/resourceGroups/example-sql-prod-aue-rg"
 
 # Assign the Definition to the Resource Group
-Write-Verbose "Assigning ARM Policy..." -Verbose
+Write-Verbose "Assigning ARM Policy..."
 
 New-AzureRmPolicyAssignment -Name OnlyConnectVMsToScaffoldVNets -DisplayName 'VMs can only connect to Scaffold VNets' `
-                            -PolicyDefinition $definition -Scope $scope -Verbose
+                            -PolicyDefinition $definition -Scope $scope
 
 
 # ## Networking
 
-Write-Verbose "Deploying Networking to networking-prod-rg..." -Verbose
+Write-Verbose "Deploying Networking to networking-prod-rg..."
 
 .\Example.ARM\Deploy-AzureResourceGroup.ps1 `
     -ResourceGroupLocation australiaeast -ResourceGroupName networking-prod-rg `
     -TemplateFile .\simple-networking\simple-networking.json `
     -TemplateParametersFile .\simple-networking\simple-networking-parameters.json `
-    -Verbose
+   
 
 
 
