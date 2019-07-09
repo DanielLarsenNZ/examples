d337f1a80d3a3e78fdc8b2d60375df8e97a7a385
Daniel Larsen <dalars@microsoft.com>
2019-07-09 20:33:14 +1200

    teardown and test file

--- solutions/appserviceplan-private-networking/deploy.ps1
+++ solutions/appserviceplan-private-networking/deploy.ps1
@@ -5,6 +5,7 @@ $plan = "helloprivate-$loc-plan"
 $tags = 'project=hello-private-networking'
 $app = "helloprivate-$loc"
 $vnet = "helloprivate-$loc-vnet"
+$storage = "helloprivate$loc"
 
 # Create resource group
 & az group create -n $rg --location $location --tags $tags
@@ -19,5 +20,13 @@ $vnet = "helloprivate-$loc-vnet"
 # Create a VNet
 & az network vnet create -n $vnet -g $rg --location $location --subnet-name default --tags $tags
 
+# Create a Storage Account
+& az storage account create -n $storage -g $rg --location $location --sku  'Standard_LRS' --https-only --kind StorageV2 --tags $tags 
+
+
+
 # Open the app in the browser
 start "https://$app.azurewebsites.net"
+
+# Tear down
+# & az group delete -n $rg --yes
