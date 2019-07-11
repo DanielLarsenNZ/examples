af0a3d4ce426541a751a4f8d8e929c0d640949ba
Daniel Larsen <dalars@microsoft.com>
2019-07-12 08:51:08 +1200

    East US

--- solutions/appserviceplan-scale-afd/deploy.ps1
+++ solutions/appserviceplan-scale-afd/deploy.ps1
@@ -1,7 +1,7 @@
 # Deploy multiple App Service Plans and loadbalance across them using Azure Front Door
 
-$location = 'australiaeast'
-$loc = 'aue'
+$location = 'eastus'
+$loc = 'eus'
 $rg = 'appserviceplanscale-rg'
 $plan1 = "appserviceplanscale1-$loc-plan"
 $plan2 = "appserviceplanscale2-$loc-plan"
@@ -9,15 +9,13 @@ $tags = 'project=appserviceplan-scale-afd'
 $app1 = "appserviceplanscale1-$loc"
 $app2 = "appserviceplanscale2-$loc"
 $frontDoor = 'appserviceplanscale'
-$insights = 'appserviceplanscale-insights'
-$insightsLocation = 'ase'
 
 # Create resource group
 az group create -n $rg --location $location --tags $tags
 
 # Create App Service Plans
-az appservice plan create -n $plan1 -g $rg --location $location --sku B1 --number-of-workers 2 --tags $tags
-az appservice plan create -n $plan2 -g $rg --location $location --sku B1 --number-of-workers 2 --tags $tags
+az appservice plan create -n $plan1 -g $rg --location $location --sku P2V2 --number-of-workers 2 --tags $tags
+az appservice plan create -n $plan2 -g $rg --location $location --sku P2V2 --number-of-workers 2 --tags $tags
 
 # Create Apps, deploy from Github
 az webapp create -n $app1 --plan $plan1 -g $rg --tags $tags `
@@ -31,12 +29,15 @@ az webapp update -n $app2 -g $rg --client-affinity-enabled false
 
 # Create Front Door
 az extension add --name front-door
-az network front-door create -n $frontDoor -g $rg --tags $tags --backend-address "$app1.azurewebsites.net" --accepted-protocols Http Https
+az network front-door create -n $frontDoor -g $rg --tags $tags `
+    --backend-address "$app1.azurewebsites.net" `
+    --accepted-protocols Http Https
 
 # Add second Backend
-az network front-door backend-pool backend add -g $rg --front-door-name $frontDoor --address "$app2.azurewebsites.net" --pool-name 'DefaultBackendPool'
-
-
+az network front-door backend-pool backend add -g $rg `
+    --front-door-name $frontDoor `
+    --address "$app2.azurewebsites.net" `
+    --pool-name 'DefaultBackendPool'
 
 # Open the apps in the browser
 start "https://$app1.azurewebsites.net"
@@ -44,4 +45,4 @@ start "https://$app2.azurewebsites.net"
 start "https://$frontDoor.azurefd.net"
 
 # Tear down
-# & az group delete -n $rg --yes
+# az group delete -n $rg --yes
