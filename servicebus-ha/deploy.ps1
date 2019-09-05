697ad0b53675894480c4bc70927ccd0594767a8a
Daniel Larsen <dalars@microsoft.com>
2019-09-05 16:43:20 +1200

    teardown switch

--- servicebus-ha/deploy.ps1
+++ servicebus-ha/deploy.ps1
@@ -1,4 +1,7 @@
 # This script deploys dual (east-west) Service Bus Premium namespaces, and pairs them
+param (
+    [Switch] $Teardown
+)
 
 $primaryLocation = 'australiaeast'
 $priLoc = 'aue'
@@ -10,22 +13,32 @@ $primaryNamespace = "servicebusha-$priLoc"
 $secondaryNamespace = "servicebusha-$secLoc"
 $alias = 'servicebusha'
 
+if ($teardown) {
+    # TEAR DOWN
+    
+    # Break pairing
+    az servicebus georecovery-alias break-pair -g $rg --namespace-name $primaryNamespace --alias $alias
+    
+    # Delete RG
+    az group delete -n $rg --yes
 
-# Create Resource Group
-az group create -n $rg --location $primaryLocation --tags $tags
+} else {
+    # DEPLOY
 
-# SERVICE BUS
-# https://docs.microsoft.com/en-us/azure/service-bus-messaging/service-bus-geo-dr#setup
-# https://docs.microsoft.com/en-us/cli/azure/servicebus/namespace?view=azure-cli-latest#az-servicebus-namespace-create
+    # Create Resource Group
+    az group create -n $rg --location $primaryLocation --tags $tags
 
-# Primary
-az servicebus namespace create -g $rg -n $primaryNamespace --location $primaryLocation --tags $tags --sku Premium --capacity 1
+    # SERVICE BUS
+    # https://docs.microsoft.com/en-us/azure/service-bus-messaging/service-bus-geo-dr#setup
+    # https://docs.microsoft.com/en-us/cli/azure/servicebus/namespace?view=azure-cli-latest#az-servicebus-namespace-create
 
-# Secondary
-az servicebus namespace create -g $rg -n $secondaryNamespace --location $secondaryLocation --tags $tags --sku Premium --capacity 1
+    # Primary
+    az servicebus namespace create -g $rg -n $primaryNamespace --location $primaryLocation --tags $tags --sku Premium --capacity 1
 
-# Set Alias and create Pair
-az servicebus georecovery-alias set -g $rg --namespace-name $primaryNamespace --alias $alias --partner-namespace $secondaryNamespace
+    # Secondary
+    az servicebus namespace create -g $rg -n $secondaryNamespace --location $secondaryLocation --tags $tags --sku Premium --capacity 1
 
-# Tear down
-# az group delete -n $rg --yes
+    # Set Alias and create Pair
+    az servicebus georecovery-alias set -g $rg --namespace-name $primaryNamespace --alias $alias --partner-namespace $secondaryNamespace
+
+}
