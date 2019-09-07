bc57fcad8767f2f9953bd36e2d1cb0fc1dd9e4c7
Daniel Larsen <dalars@microsoft.com>
2019-09-08 10:38:39 +1200

    Function example for App insights dependency tracking (WIP)

---
+++ webjobs-eventhubs/Examples.Pipeline.Functions/Function1.cs
@@ -0,0 +1,17 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace Examples.Pipeline.Functions
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        [return: EventHub("outputEventHubMessage", Connection = "EventHubConnectionAppSetting")]
+        public static void Run([ServiceBusTrigger("myqueue", Connection = "")]string myQueueItem, ILogger log)
+        {
+            log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+        }
+    }
+}
