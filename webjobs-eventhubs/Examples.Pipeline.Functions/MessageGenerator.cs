c79eb6e99d9246e66a206d4d349789afcd07aabc
Daniel Larsen <dalars@microsoft.com>
2019-09-16 09:39:09 +1200

    Message genny in a Function :\

---
+++ webjobs-eventhubs/Examples.Pipeline.Functions/MessageGenerator.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+
+namespace Examples.Pipeline.Functions
+{
+    public static class MessageGenerator
+    {
+        private static readonly Lazy<QueueClient> _lazyQueueClient = new Lazy<QueueClient>(InitializeQueueClient);
+        private static QueueClient QueueClient => _lazyQueueClient.Value;
+        private static IConfiguration _config;
+
+
+        [FunctionName("MessageGenerator")]
+        public static async Task Run([TimerTrigger("0 */1 * * * *")]TimerInfo timer, 
+            ILogger log,
+            ExecutionContext context)
+        {
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+
+            const int rpm = 60;
+
+            _config = new ConfigurationBuilder()
+                .SetBasePath(context.FunctionAppDirectory)
+                .AddJsonFile("local.settings.json", optional: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            try
+            {
+                for (var i = 0; i < rpm; i++)
+                {
+                    // Create a new message to send to the queue.
+                    string messageBody = $"Message {i}";
+                    var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+
+                    // Write the body of the message to the console.
+                    Console.WriteLine($"Sending message: {messageBody}");
+
+                    // Send the message to the queue.
+                    await QueueClient.SendAsync(message);
+
+                    await Task.Delay(60000 / rpm);
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
+            }
+        }
+
+        private static QueueClient InitializeQueueClient()
+        {
+            return new QueueClient(_config["ServiceBusConnectionString"], _config["MessageGenerator.QueueName"]);
+        }
+    }
+}
--- webjobs-eventhubs/deploy.ps1
+++ webjobs-eventhubs/deploy.ps1
@@ -3,6 +3,10 @@
 # and Hub and a Service Bus namespace and queues.
 # It also deploys a WebJob App and a Function App.
 
+param (
+    [Switch] $SkipDeploy
+)
+
 $location = 'australiaeast'
 $loc = 'aue'
 $rg = 'hellomessaging-rg'
@@ -30,57 +34,58 @@ $eventhubsPartitions = 2    # 2 - 32. Cannot be changed after deployment. Good d
 $servicebusSku = 'Basic'
 
 
-# Create Resource Group
-az group create -n $rg --location $location --tags $tags
+if (!$SkipDeploy) {
+    # Create Resource Group
+    az group create -n $rg --location $location --tags $tags
 
 
-# STORAGE ACCOUNTS
-# https://docs.microsoft.com/en-us/cli/azure/storage/account?view=azure-cli-latest#az-storage-account-create
-az storage account create -n $webjobsStorage -g $rg -l $location --tags $tags --sku Standard_LRS
-az storage account create -n $dataStorage -g $rg -l $location --tags $tags --sku Standard_LRS
+    # STORAGE ACCOUNTS
+    # https://docs.microsoft.com/en-us/cli/azure/storage/account?view=azure-cli-latest#az-storage-account-create
+    az storage account create -n $webjobsStorage -g $rg -l $location --tags $tags --sku Standard_LRS
+    az storage account create -n $dataStorage -g $rg -l $location --tags $tags --sku Standard_LRS
 
-$webjobsStorageConnection = ( az storage account show-connection-string -g $rg -n $webjobsStorage | ConvertFrom-Json ).connectionString
-$dataStorageConnection = ( az storage account show-connection-string -g $rg -n $dataStorage | ConvertFrom-Json ).connectionString
-
-
-# APPLICATION INSIGHTS
-#  https://docs.microsoft.com/en-us/cli/azure/ext/application-insights/monitor/app-insights/component?view=azure-cli-latest
-az extension add -n application-insights
-
-$instrumentationKey = ( az monitor app-insights component create --app $insights --location $location -g $rg --tags $tags | ConvertFrom-Json ).instrumentationKey
+    $webjobsStorageConnection = ( az storage account show-connection-string -g $rg -n $webjobsStorage | ConvertFrom-Json ).connectionString
+    $dataStorageConnection = ( az storage account show-connection-string -g $rg -n $dataStorage | ConvertFrom-Json ).connectionString
 
 
-# APP SERVICES
-# Create App Service Plan
-az appservice plan create -n $plan -g $rg --location $location --sku $planSku --number-of-workers $planInstances --tags $tags
+    # APPLICATION INSIGHTS
+    #  https://docs.microsoft.com/en-us/cli/azure/ext/application-insights/monitor/app-insights/component?view=azure-cli-latest
+    az extension add -n application-insights
 
-# Create WebJob app
-az webapp create -n $webjobApp --plan $plan -g $rg --tags $tags
+    $instrumentationKey = ( az monitor app-insights component create --app $insights --location $location -g $rg --tags $tags | ConvertFrom-Json ).instrumentationKey
 
-# Configure always on
-az webapp config set -n $webjobApp -g $rg --always-on true
 
-# Package and zip the WebJob
-dotnet publish .\Examples.Pipeline.WebJobs\ --configuration Release -o '../_zip/app_data/Jobs/Continuous/Examples.Pipeline.Webjobs'
-copy ./run.cmd './_zip/app_data/Jobs/Continuous/Examples.Pipeline.Webjobs'
-Compress-Archive -Path ./_zip/* -DestinationPath ./deploy.zip -Force
+    # APP SERVICES
+    # Create App Service Plan
+    az appservice plan create -n $plan -g $rg --location $location --sku $planSku --number-of-workers $planInstances --tags $tags
 
-# Deploy source code
-az webapp deployment source config-zip -g $rg -n $webjobApp --src ./deploy.zip
+    # Create WebJob app
+    az webapp create -n $webjobApp --plan $plan -g $rg --tags $tags
 
-# FUNCTION APP
-az functionapp create -n $functionApp --plan $plan -g $rg --tags $tags -s $webjobsStorage --app-insights $insights --app-insights-key $instrumentationKey
+    # Configure always on
+    az webapp config set -n $webjobApp -g $rg --always-on true
 
-# Configure always on
-az functionapp config set -n $functionApp -g $rg --always-on true
+    # Package and zip the WebJob
+    dotnet publish .\Examples.Pipeline.WebJobs\ --configuration Release -o '../_zip/app_data/Jobs/Continuous/Examples.Pipeline.Webjobs'
+    copy ./run.cmd './_zip/app_data/Jobs/Continuous/Examples.Pipeline.Webjobs'
+    Compress-Archive -Path ./_zip/* -DestinationPath ./deploy.zip -Force
 
-# Package and zip the Function App
-dotnet publish .\Examples.Pipeline.Functions\ --configuration Release -o '../_functionzip'
-Compress-Archive -Path ./_functionzip/* -DestinationPath ./deployfunction.zip -Force
+    # Deploy source code
+    az webapp deployment source config-zip -g $rg -n $webjobApp --src ./deploy.zip
 
-# Deploy source code
-az functionapp deployment source config-zip -g $rg -n $functionApp --src ./deployfunction.zip
+    # FUNCTION APP
+    az functionapp create -n $functionApp --plan $plan -g $rg --tags $tags -s $webjobsStorage --app-insights $insights --app-insights-key $instrumentationKey
 
+    # Configure always on
+    az functionapp config set -n $functionApp -g $rg --always-on true
+
+    # Package and zip the Function App
+    dotnet publish .\Examples.Pipeline.Functions\ --configuration Release -o '../_functionzip'
+    Compress-Archive -Path ./_functionzip/* -DestinationPath ./deployfunction.zip -Force
+
+    # Deploy source code
+    az functionapp deployment source config-zip -g $rg -n $functionApp --src ./deployfunction.zip
+}
 
 # EVENT HUBS
 # https://docs.microsoft.com/en-us/cli/azure/eventhubs?view=azure-cli-latest
@@ -92,10 +97,10 @@ foreach ($eventhub in $eventhubs) {
     az eventhubs eventhub create -g $rg --namespace-name $eventhubNamespace --name $eventhub --message-retention $eventhubsRetentionDays --partition-count $eventhubsPartitions
 }
 
-az eventhubs eventhub authorization-rule create -g $rg --namespace-name $eventhubNamespace --eventhub-name $eventhub --name $eventhubAuthRule --rights Listen Send
+az eventhubs namespace authorization-rule create -g $rg --namespace-name $eventhubNamespace --name $eventhubAuthRule --rights Listen Send
 
 # Get connection string
-$eventhubConnectionString = ( az eventhubs eventhub authorization-rule keys list --resource-group $rg --namespace-name $eventhubnamespace --eventhub-name $eventhub --name $eventhubauthrule | ConvertFrom-Json ).primaryConnectionString
+$eventhubConnectionString = ( az eventhubs namespace authorization-rule keys list --resource-group $rg --namespace-name $eventhubnamespace --name $eventhubauthrule | ConvertFrom-Json ).primaryConnectionString
 
 # SERVICE BUS
 # https://docs.microsoft.com/en-us/cli/azure/servicebus/namespace?view=azure-cli-latest#az-servicebus-namespace-create
@@ -118,7 +123,7 @@ az webapp config appsettings set -n $webjobApp -g $rg --settings "APPINSIGHTS_IN
 
 az functionapp config appsettings set -n $functionApp -g $rg --settings "APPINSIGHTS_INSTRUMENTATIONKEY=$instrumentationKey" "AzureWebJobsStorage=$webjobsStorageConnection" "DataStorageConnectionString=$dataStorageConnection" "EventHubConnectionString=$eventhubConnectionString" "ServiceBusConnectionString=$servicebusConnectionString"
 
-
+<#
 # Count to 10
 Start-Sleep 10
 
@@ -126,6 +131,8 @@ Start-Sleep 10
 # https://mockaroo.com/
 az storage container create -n 'data' --account-name $dataStorage --public-access off
 az storage blob upload --account-name $datastorage -f ./transactions.csv -c $container -n 'transactions.csv'
+
+#>
 
 # Tear down
 # az group delete -n $rg --yes
