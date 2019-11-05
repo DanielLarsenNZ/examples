7adfa6fed842c7ca9b091defb05081fea637002d
Daniel Larsen <dalars@microsoft.com>
2019-11-05 14:06:21 +1300

    It's 2019

--- appservice-appinsights/deploy.ps1
+++ appservice-appinsights/deploy.ps1
@@ -55,7 +55,7 @@ start "https://$app.azurewebsites.net/"
 
 MIT License
 
-Copyright (c) 2017 Daniel Larsen
+Copyright (c) 2019 Daniel Larsen
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
