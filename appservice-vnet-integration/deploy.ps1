8032767bc5e07a9cc174cc66eeb530ab317f9138
Daniel Larsen <dalars@microsoft.com>
2020-03-17 14:23:07 +1300

    App Service with Regional VNet integration

---
+++ appservice-vnet-integration/deploy.ps1
@@ -0,0 +1,12 @@
+$rg = 'hellovnet-rg'
+$location = 'australiaeast'
+$tags = @{ 
+    expires = [DateTime]::UtcNow.AddDays(7); 
+    owner = "dalars"; 
+    project = "appservice-vnet-integration" 
+}
+
+$ErrorActionPreference = 'Stop'
+
+New-AzResourceGroup -Name $rg -Location $location -Tag $tags -Force -Verbose
+New-AzResourceGroupDeployment -ResourceGroupName $rg -Mode 'Incremental' -TemplateFile (Join-Path $PSScriptRoot './armdeploy.json') -Verbose
